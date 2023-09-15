962e7c48053215a029e07f1527999f3b324273a4
Rageking8 <tomleetyt@gmail.com>
2023-09-16 02:06:06 +0800

    Add `UnconditionalTransition` class

---
+++ Console/HSM/Core/CustomTransitions/UnconditionalTransition.cs
@@ -0,0 +1,50 @@
+namespace HSM.Core.CustomTransitions
+{
+    using System;
+
+    using HSM.Core.Transition;
+
+    // Transition that will take place without any conditions
+    public class UnconditionalTransition<TStateID> :
+        Transition<TStateID>
+    {
+        public UnconditionalTransition(TStateID fromStateID,
+            TStateID toStateID) :
+            base(fromStateID, toStateID)
+        {
+
+        }
+
+        public UnconditionalTransition(TStateID fromStateID,
+            TStateID toStateID, Action callback) :
+            this(fromStateID, toStateID)
+        {
+            SetCallback(callback);
+        }
+
+        public override bool Conditions()
+        {
+            return true;
+        }
+    }
+
+    // A helper class `UnconditionalTransition` with the
+    // generic argument defaulted to a common type
+    public class UnconditionalTransition :
+        UnconditionalTransition<string>
+    {
+        public UnconditionalTransition(string fromStateID,
+            string toStateID) :
+            base(fromStateID, toStateID)
+        {
+
+        }
+
+        public UnconditionalTransition(string fromStateID,
+            string toStateID, Action callback) :
+            base(fromStateID, toStateID, callback)
+        {
+
+        }
+    }
+}
