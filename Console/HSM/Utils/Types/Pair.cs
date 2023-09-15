096ed92a0abf70829cfa6e1e59cc7af9714d2373
Rageking8 <tomleetyt@gmail.com>
2023-09-16 02:25:39 +0800

    Add new `Pair` util type

---
+++ Console/HSM/Utils/Types/Pair.cs
@@ -0,0 +1,50 @@
+namespace HSM.Utils.Types
+{
+    using System;
+
+    // Simple pair holding 2 values
+    // of possibly distinct types
+    [Serializable]
+    public class Pair<K, V>
+    {
+        public K First { get; set; }
+
+        public V Second { get; set; }
+
+        // Allow access via `Key`
+        public K Key
+        {
+            get => First;
+            set => First = value;
+        }
+
+        // Allow access via `Value`
+        public V Value
+        {
+            get => Second;
+            set => Second = value;
+        }
+
+        public Pair(K first = default, V second = default)
+        {
+            First = first;
+            Second = second;
+        }
+    }
+
+    // A simple helper class where both
+    // the generic arguments are the same
+    [Serializable]
+    public class Pair<T> : Pair<T, T>
+    {
+
+    }
+
+    // A simple helper class with the first
+    // generic argument set to string
+    [Serializable]
+    public class PairStr<T> : Pair<string, T>
+    {
+
+    }
+}
