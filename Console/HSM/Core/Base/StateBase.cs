92cf6526efecc4b78f0108c2d2810e13d3a94764
Rageking8 <tomleetyt@gmail.com>
2023-09-16 00:34:07 +0800

    Create `SMChild`, `StateBase` and `IStateMachine`

---
+++ Console/HSM/Core/Base/StateBase.cs
@@ -0,0 +1,74 @@
+namespace HSM.Core.Base
+{
+    using HSM.Core.Interfaces;
+
+    using static HSM.Utils.DebugUtils;
+
+    // Base abstract class shared between
+    // an actual `State` and a `StateMachine`
+    public abstract class StateBase<TStateID> :
+        SMChild<TStateID>, ISealable
+    {
+        public readonly TStateID StateID;
+
+        // Gets whether the current object is already sealed
+        public bool IsSealed { get; protected set; }
+
+        // Gets whether the current object is ready to be sealed
+        public bool CanSeal { get; protected set; }
+
+        protected StateBase(TStateID stateID)
+        {
+            Assert(stateID != null,
+                "`stateID` is null");
+
+            StateID = stateID;
+        }
+
+        public virtual void Enter()
+        {
+
+        }
+
+        public virtual void Update()
+        {
+
+        }
+
+        public virtual void FixedUpdate()
+        {
+
+        }
+
+        public virtual void LateUpdate()
+        {
+
+        }
+
+        public virtual void Exit()
+        {
+
+        }
+
+        public virtual void Seal()
+        {
+            Assert(CanSeal, "Seal operation is invalid");
+            Assert(!IsSealed, "Attempted to reseal");
+
+            IsSealed = true;
+        }
+
+        protected abstract void InternalCheckSeal();
+    }
+
+    // A helper abstract class `StateBase` with the generic
+    // argument defaulted to a common type
+    public abstract class StateBase : StateBase<string>
+    {
+        protected StateBase(string stateID) :
+            base(stateID)
+        {
+
+        }
+    }
+}
---
+++ Console/HSM/Core/Interfaces/IStateMachine.cs
@@ -0,0 +1,21 @@
+﻿namespace HSM.Core.Interfaces
+{
+    using HSM.Core.Base;
+
+    // Used to tell apart a regular state from
+    // a state machine and also provides functionality
+    // only present in a state machine
+    public interface IStateMachine<TStateID>
+    {
+        // Get the current state this state machine is in
+        StateBase<TStateID> CurrentState { get; }
+    }
+
+    // A helper interface `IStateMachine` with the generic
+    // argument defaulted to a common type
+    public interface IStateMachine :
+        IStateMachine<string>
+    {
+
+    }
+}
