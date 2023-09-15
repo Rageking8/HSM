e4e50cd626097415f35b624bdf82776f2e9fa6f3
Rageking8 <tomleetyt@gmail.com>
2023-09-16 00:20:15 +0800

    Add `DelayUtils` static class

---
+++ Console/HSM/Utils/DelayUtils.cs
@@ -0,0 +1,15 @@
+﻿namespace HSM.Utils
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public static class Delay
+    {
+        public static async Task Execute(Action action,
+            float delay)
+        {
+            await Task.Delay((int)(delay * 1000f));
+            action();
+        }
+    }
+}
