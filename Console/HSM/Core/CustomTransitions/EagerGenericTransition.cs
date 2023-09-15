87d93a21a6082c33afb9c9761541460fb0946563
Rageking8 <tomleetyt@gmail.com>
2023-09-16 02:05:06 +0800

    Add `EagerGenericTransition` class

---
+++ Console/HSM/Core/CustomTransitions/EagerGenericTransition.cs
@@ -0,0 +1,72 @@
+namespace HSM.Core.CustomTransitions
+{
+    using System;
+    using System.Collections.Generic;
+
+    using HSM.Core.Interfaces;
+
+    using static HSM.Utils.ListUtils;
+
+    // Use of eager transitions need to be done in caution, as
+    // careless usage can lead to infinite loops where 2 or more
+    // states eagerly transit to each other forming a closed cycle
+    //
+    // SAFETY: Long eager transition loops cannot be relied upon
+    // with the current implementation (with recursion) as it will
+    // stack overflow
+    public class EagerGenericTransition<TStateID> :
+        GenericTransition<TStateID>, IEagerTransition
+    {
+        private List<Func<bool>> _eagerConditions = new();
+
+        public EagerGenericTransition(TStateID fromStateID,
+            TStateID toStateID, params Func<bool>[] initConditions) :
+            base(fromStateID, toStateID, initConditions)
+        {
+
+        }
+
+        public EagerGenericTransition(TStateID fromStateID,
+            TStateID toStateID, Action callback,
+            params Func<bool>[] initConditions) :
+            this(fromStateID, toStateID, initConditions)
+        {
+            SetCallback(callback);
+        }
+
+        public bool IsEager()
+        {
+            bool? result = _eagerConditions.AllTrue();
+
+            // Take empty `eagerConditions` (`result` is null)
+            // as always true
+            return result == null || (bool)result;
+        }
+
+        public void AddEagerCondition(Func<bool> eagerCondition)
+        {
+            _eagerConditions.Add(eagerCondition);
+        }
+    }
+
+    // A helper class `EagerGenericTransition` with the generic
+    // argument defaulted to a common type
+    public class EagerGenericTransition :
+        EagerGenericTransition<string>
+    {
+        public EagerGenericTransition(string fromStateID,
+            string toStateID, params Func<bool>[] initConditions) :
+            base(fromStateID, toStateID, initConditions)
+        {
+
+        }
+
+        public EagerGenericTransition(string fromStateID,
+            string toStateID, Action callback,
+            params Func<bool>[] initConditions) :
+            base(fromStateID, toStateID, callback, initConditions)
+        {
+
+        }
+    }
+}
