f10997f129cfc25ae626695be50fcc34ca49a88b
Rageking8 <tomleetyt@gmail.com>
2023-09-16 02:47:17 +0800

    Create `RepeatUtils` helper static class

---
+++ Console/HSM/Utils/Helpers/RepeatUtils.cs
@@ -0,0 +1,23 @@
+﻿#if HSM_CONSOLE
+namespace HSM.Utils.Helpers
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    // Only for use in console versions
+    public static class RepeatUtils
+    {
+        public static async Task InvokeLoop(Action action, float duration)
+        {
+            PeriodicTimer timer =
+                new(TimeSpan.FromSeconds(duration));
+
+            while (await timer.WaitForNextTickAsync())
+            {
+                action();
+            }
+        }
+    }
+}
+#endif
